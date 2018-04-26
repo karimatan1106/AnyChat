966af64bd4ff5c3dfbe1f0d63c1138859b6594f7
karimata1106 <karimata1106@gmail.com>
2018-04-26 22:36:57 +0900

    通知をクリックした際に該当のウィンドウを開きたい #4

--- AnyChat/Scripts/signalR-dispatch.js
+++ AnyChat/Scripts/signalR-dispatch.js
@@ -26,10 +26,11 @@
             Push.create(speech, {
                 //body: '更新をお知らせします！',
                 //icon: 'icon.png',
-                timeout: 3000, // 通知が消えるタイミング
+                timeout: 5000, // 通知が消えるタイミング
                 vibrate: [100, 100, 100], // モバイル端末でのバイブレーション秒数
                 onClick: function () {
                     // 通知がクリックされた場合の設定
+                    window.focus();
                     this.close();
                 }
             });
