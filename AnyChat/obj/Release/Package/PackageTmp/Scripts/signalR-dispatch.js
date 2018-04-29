712a9f726d9b615999c8f09db1243821491bcbc8
karimata1106 <karimata1106@gmail.com>
2018-04-29 17:46:52 +0900

    検索ボックスの下のタブをMaterial Componentsに差し替える #1

--- AnyChat/obj/Release/Package/PackageTmp/Scripts/signalR-dispatch.js
+++ AnyChat/obj/Release/Package/PackageTmp/Scripts/signalR-dispatch.js
@@ -26,10 +26,11 @@
             Push.create(speech, {
                 //body: '更新をお知らせします！',
                 //icon: 'icon.png',
-                timeout: 3000, // 通知が消えるタイミング
+                timeout: 5000, // 通知が消えるタイミング
                 vibrate: [100, 100, 100], // モバイル端末でのバイブレーション秒数
                 onClick: function () {
                     // 通知がクリックされた場合の設定
+                    window.focus();
                     this.close();
                 }
             });
@@ -49,11 +50,16 @@
     //セッションタイムアウトした場合に再接続を行う
     connection.disconnected(function () {
         setTimeout(function () {
-            $.connection.hub.start();
+            connection.start().done(function () {
+            }).fail(function (error) {
+                console.log('Invocation of start failed. Error:' + error)
+            });
         }, 5000); // Restart connection after 5 seconds.
     });
 
     //接続を開始
-    connection.start(function () {
+    connection.start().done(function () {
+    }).fail(function (error) {
+        console.log('Invocation of start failed. Error:' + error)
     });
 });
